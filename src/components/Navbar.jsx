c71695ec58f5534e019b4f5e1c872a3ea3a25ea7
Nwaokolo Ikechukwu <105738441+Pinewavez@users.noreply.github.com>
2022-06-24 18:11:04 +0100

    Added useState for NavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,34 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Logo from '../assets/pineArtsLogo.png'
 import {NavLink} from 'react-router-dom';
 import '../styles/Navbar.css';
+import {FaBars} from 'react-icons/fa'
 
 function Navbar() {
+  const [isOpen, opener] = useState(false)
+  const toggleNav = () => {
+    opener(!isOpen)
+  };
   return (
+
     <div className="navBar">
         <div className='leftSide'>
             <img src={Logo} className='logo' alt='logo' />
         </div>
+
+        <div className='hiddenLinks' id={ isOpen ? "open" : "closed"}>
+           <NavLink to={"/"}>Home</NavLink>
+           <NavLink to={"/gallery"}>Gallery</NavLink>
+           <NavLink to={"/about"}>About</NavLink>
+           <NavLink to={"/contact"}>Contact</NavLink>
+        </div>
+
         <div className='rightSide'>
            <NavLink to={"/"}>Home</NavLink>
            <NavLink to={"/gallery"}>Gallery</NavLink>
            <NavLink to={"/about"}>About</NavLink>
            <NavLink to={"/contact"}>Contact</NavLink>
-           <button></button>
+           <button onClick={toggleNav}><FaBars/></button>
         </div>
     </div>
   )
