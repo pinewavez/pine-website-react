840ed0232633a8add8ddd2f99715787a9cab9131
Nwaokolo Ikechukwu <105738441+Pinewavez@users.noreply.github.com>
2022-06-29 00:00:35 +0100

    Updated Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,37 +3,32 @@ import Logo from '../assets/pineArtsLogo.png'
 import {NavLink} from 'react-router-dom';
 import '../styles/Navbar.css';
 import {FaBars} from 'react-icons/fa'
+// import {FaWindowClose} from 'react-icons/fa'
+
 
 function Navbar() {
-  const [isOpen, setOpen] = useState(true)
+  const [isOpen, setOpen] = useState(false)
   const toggleNav = () => {
     setOpen(!isOpen)
   };
   return (
 
     <div className="navBar">
-        <div className='leftSide' id={ isOpen ? "open" : "closed"}>
+        <div className='leftSide'>
             <img src={Logo} className='logo' alt='logo' />
-
-        <div className='hiddenLinks'>
-           <NavLink to={"/"}>Home</NavLink>
-           <NavLink to={"/gallery"}>Gallery</NavLink>
-           <NavLink to={"/about"}>About</NavLink>
-           <NavLink to={"/contact"}>Contact</NavLink>
-        </div>
         </div>
 
-        
-
-        <div className='rightSide'>
-          <ul>
-            <li><NavLink to={"/"}>Home</NavLink></li>
-            <li><NavLink to={"/gallery"}>Gallery</NavLink></li>
-            <li><NavLink to={"/about"}>About</NavLink></li>
-            <li><NavLink to={"/contact"}>Contact</NavLink></li>
-          </ul>
-           
-           <button onClick={toggleNav}><FaBars/></button>
+        <div className="rightSide">
+          <div className='links' id={isOpen ? "open" : "closed"}> 
+            <ul>
+              <li><NavLink to={"/"}>Home</NavLink></li>
+              <li><NavLink to={"/gallery"}>Gallery</NavLink></li>
+              <li><NavLink to={"/about"}>About</NavLink></li>
+              <li><NavLink to={"/contact"}>Contact</NavLink></li>
+            </ul>
+          </div>
+          <button className="open" onClick={toggleNav}><FaBars/></button>
+          {/* <button className="close" onClick={toggleNav}><FaWindowClose/></button> */}
         </div>
     </div>
   )
